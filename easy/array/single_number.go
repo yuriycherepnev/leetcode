857b8c68081014ae0be24ef256ca46d3f18e8e94
yuriy <yuriy.cherepnev@gmail.com>
2026-04-20 14:17:24 +0300

    1111

--- easy/array/single_number.go
+++ easy/array/single_number.go
@@ -5,16 +5,39 @@ import "fmt"
 func main() {
 	nums := []int{2, 1, 4, 1, 2}
 	single := singleNumber(nums)
-
 	fmt.Println(single)
 }
 
+// оба варианта решения верны
+// решение через XOR
 func singleNumber(nums []int) int {
+	number := 0
+
+	for _, value := range nums {
+		number = number ^ value
+	}
+
+	return number
+}
+
+// решение через map
+/*
+func singleNumber(nums []int) int {
+	numbers := make(map[int]struct{})
 	result := 0
 
 	for _, num := range nums {
-		result = result ^ num
+		_, ok := numbers[num]
+		if !ok {
+			numbers[num] = struct{}{}
+		} else {
+			delete(numbers, num)
+		}
 	}
 
+	for key := range numbers {
+		result = key
+	}
 	return result
 }
+*/
