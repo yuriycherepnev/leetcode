e12cc1926432d65b2226f738712703975a9f04d6
yuriy <yuriy.cherepnev@gmail.com>
2026-04-28 10:45:11 +0300

    5555

--- easy/array/intersection_of_arrays.go
+++ easy/array/intersection_of_arrays.go
@@ -12,14 +12,37 @@ import (
 
 func main() {
 	nums1 := []int{5, 2, 8, 1, 9, 3, 8}
-	nums2 := []int{2, 4, 6, 8, 8, 9, 10, 1000, 1002, 1004, 2000}
+	nums2 := []int{4, 6, 8, 8, 9, 10, 1000, 1002, 1004, 2000, 2}
 
-	result := intersect(nums1, nums2)
+	result := mapIntersect(nums1, nums2)
 
 	fmt.Println(result)
 }
 
-func intersect(nums1 []int, nums2 []int) []int {
+/*
+решение через мапы
+*/
+func mapIntersect(nums1 []int, nums2 []int) []int {
+	mapNums := make(map[int]int)
+
+	for _, v := range nums1 {
+		mapNums[v]++
+	}
+
+	result := make([]int, 0, len(nums1))
+	for _, v := range nums2 {
+		if mapNums[v] > 0 {
+			mapNums[v]--
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+/*
+решение через сортировку слайсов
+*/
+func sortIntersect(nums1 []int, nums2 []int) []int {
 	sort.Ints(nums1)
 	sort.Ints(nums2)
 
