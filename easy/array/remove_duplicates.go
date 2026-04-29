1189bcfc1fcfc7e3e004f043d2aea3cb0f5efec8
yuriy <yuriy.cherepnev@gmail.com>
2026-04-29 17:13:40 +0300

    56555

--- easy/array/remove_duplicates.go
+++ easy/array/remove_duplicates.go
@@ -1,4 +1,16 @@
-// [0,0,1,1,1,2,2,3,3,4]
+/*
+Given an integer array nums sorted in non-decreasing order,
+remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k.
+After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order.
+The remaining elements beyond index k - 1 can be ignored.
+*/
+// input [0,0,1,1,1,2,2,3,3,4]
+// output [0 1 2 3 4 2 3 3 4] or [0 1 2 3 4 _ _ _ _]
 package main
 
 import "fmt"
