a5e2f01bb72518c1443c93588197a5bd7a3cb7e1
yuriy <yuriy.cherepnev@gmail.com>
2026-04-30 14:13:34 +0300

    5555

--- easy/array/plus_one.go
+++ easy/array/plus_one.go
@@ -1,35 +1,30 @@
+/*
+You are given a large integer represented as an integer array digits,
+where each digits[i] is the ith digit of the integer.
+The digits are ordered from most significant to least significant in left-to-right order.
+The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+*/
 package main
 
 import "fmt"
 
 func main() {
-	//digits := []int{1, 2, 3, 4}
-
-	//plusOne(digits)
-
-	number := 12345
-
-	remainder := number % 10000
-
-	number /= 10000
-
-	fmt.Println(number)
-	fmt.Println(remainder)
+	digits := []int{9, 9, 9, 9}
+	result := plusOne(digits)
+	fmt.Println(result)
 }
 
-func plusOne(digits []int) int {
-	length := len(digits)
-	increase := 1
-	number := 0
-
-	for i := length - 1; i >= 0; i-- {
-		number += digits[i] * increase
-		increase *= 10
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
 	}
-
-	fmt.Println(increase)
-
-	fmt.Println(number)
-
-	return number
+	result := make([]int, len(digits)+1)
+	result[0] = 1
+	return result
 }
