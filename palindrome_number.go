97dda8c2d7f0f2e05ee0c43dafbc998ba25473f7
yuriy <yuriy.cherepnev@gmail.com>
2026-08-27 19:29:00 +0300

    111

--- palindrome_number.go
+++ palindrome_number.go
@@ -3,38 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	number := 10
-	result := isPalindromeTwo(number)
+	number := 900009
+	result := isPalindrome(number)
 	fmt.Println(result)
 }
 
 // сравнение всего числа
 func isPalindrome(x int) bool {
-	if x < 0 {
-		return false
+	reversed := 0
+
+	for x > reversed {
+		remain := x % 10
+		reversed = reversed*10 + remain
+		x /= 10
 	}
 
-	divider := 1
-
-	for temp := x / 10; temp != 0; temp /= 10 {
-		divider *= 10
-	}
-
-	for x > 0 {
-		first := x / divider
-		last := x % 10
-
-		fmt.Println(first, last)
-
-		if first != last {
-			return false
-		}
-
-		x = (x % divider) / 10
-		divider /= 100
-	}
-
-	return true
+	return reversed == x
 }
 
 // сравнение половины числа
