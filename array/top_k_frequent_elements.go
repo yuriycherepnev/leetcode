a1f698deb2a9972c5525a44f4828b57499252a0e
yuriy <yuriy.cherepnev@gmail.com>
2026-09-02 15:31:20 +0300

    111

--- array/top_k_frequent_elements.go
+++ array/top_k_frequent_elements.go
@@ -39,31 +39,3 @@ func topKFrequent(nums []int, k int) []int {
 
 	return result
 }
-
-func topKFrequentMap(nums []int, k int) []int {
-	mapNums := make(map[int]int)
-
-	for _, v := range nums {
-		mapNums[v]++
-	}
-
-	result := make([]int, k)
-
-	i := 0
-	for k > 0 {
-		maxValue := 0
-		maxKey := 0
-		for key, value := range mapNums {
-			if value > maxValue {
-				maxValue = value
-				maxKey = key
-			}
-		}
-		result[i] = maxKey
-		delete(mapNums, maxKey)
-		k--
-		i++
-	}
-
-	return result
-}
